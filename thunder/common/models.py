fa69c351e7b3cb26a3bebd03b1636e25adb251ff
meliu <wenkai.mel@gmail.com>
2012-03-19 17:20:49 +0800

    patch about sign

--- thunder/common/models.py
+++ thunder/common/models.py
@@ -1,4 +1,6 @@
-from thunder.model import Base
+import sys
+sys.path.append('../..')
+from thunder.model import Base, DB
 
 tables = '''
 CREATE DATABASE IF NOT EXISTS `thunder`;
@@ -17,60 +19,108 @@ PRIMARY KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `pictures`;
-CREATE TABLE `pcitures` (
+CREATE TABLE `pictures` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(30) NOT NULL,
 `ourl` varchar(100) NOT NULL,
 `surl` varchar(100) NOT NULL,
-`create_time` int(10) NOT NULL DEFAULT '0',
-`update_time` int(10) DEFAUTL '0',
+`create_time` int(10) DEFAULT '0',
+`update_time` int(10) DEFAULT '0',
 PRIMARY KEY (`id`),
-UNIQUE KEY `name` (`name`),
-UNIQUE KEY `ourl` (`ourl`),
-UNIQUE KEY `surl` (`surl`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=ut8;
+UNIQUE KEY (`name`),
+UNIQUE KEY (`ourl`),
+UNIQUE KEY (`surl`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `person_info`;
-CREATE TABLE `user_info` (
+CREATE TABLE `person_info` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `gender` int(1) NOT NULL,
 `lastname` varchar(50) NOT NULL,
 `firstname` varchar(50) NOT NULL,
 `user_id` int(11) NOT NULL,
+`create_time` int(10) DEFAULT '0',
+`update_time` int(10) DEFAULT '0',
 PRIMARY KEY (`id`),
 KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=ut8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `firm_info`;
 CREATE TABLE `firm_info` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(50) NOT NULL,
 `user_id` int(11) NOT NULL,
+`create_time` int(10) DEFAULT '0',
+`update_time` int(10) DEFAULT '0',
 PRIMARY KEY (`id`),
 KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=ut8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `person_vip';
+CREATE TABLE `person_vip` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`right` int(3) DEFAULT '0',
+`create_time` int(10) DEFAULT '0',
+`update_time` int(10) DEFAULT '0',
+PRIMARY KEY (`id`)
+)
+
+DROP TABLE IF EXISTS `firm_vip';
+CREATE TABLE `firm_vip` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`right` int(3) DEFAULT '0',
+`create_time` int(10) DEFAULT '0',
+`update_time` int(10) DEFAULT '0',
+PRIMARY KEY (`id`)
+)
 '''
 
 class User(Base):
+    '''
+    cid 1: common person, 2: vip person, 3: firm, 4: vip firm
+    '''
     __table__ = 'users'
     
-    def __init__(self, email, password):
+    def __init__(self, email, password, cid):
         item = dict(
                 email = email,
                 password = password,
+                cid = cid
             )
         Base.__init__(self, item)
 
 class PersonInfo(Base):
     __table__ = 'person_info'
 
-    def __init__(self, gender, lastname, firstname):
+    def __init__(self, gender, lastname, firstname, user_id):
         item = dict(
                 gender = gender,
                 lastname = lastname,
-                firstname = firstname
+                firstname = firstname,
+                user_id = user_id
             )
         Base.__init__(self, item)
+
+class FirmInfo(Base):
+    __table__ = 'firm_info'
+
+    def __init__(self, name):
+        item = dict(
+                name = name,
+            )
+        Base.__init__(self, item)
+
+class PersonVIP(Base):
+    __table__ = 'person_vip'
+
+    def __init__(self, right):
+        Base.__init__(self, dict(right=right))
+
+class FirmVIP(Base):
+    __table__ = 'firm_vip'
+
+    def __init__(self, right):
+        Base.__init__(self, dict(right=right))
 
 class Picture(Base):
     __table__ = 'pictures'
@@ -82,3 +132,7 @@ class Picture(Base):
                 surl = surl,
             )
         Base.__init__(self, item)
+
+if __name__ == '__main__':
+    DB._create_table(tables)
+
--- thunder/handler.py
+++ thunder/handler.py
@@ -1,4 +1,7 @@
+import hashlib
 import tornado
+from thunder.model import Query
+from thunder.common.models import User, PersonInfo, FirmInfo, PersonVIP, FirmVIP
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -6,3 +9,62 @@ class BaseHandler(tornado.web.RequestHandler):
         if not user:
             return None
         return tornado.escape.json_decode(user)
+
+class SignHandler(tornado.web.RequestHandler):
+    def verify_user(self, email, password):
+        r = {}
+        errors = []
+        if not email:
+            errors.append('email_blank')
+        if not password:
+            errors.append('password_blank')
+
+        if email and password:
+            user = Query(User).filter_by(email=email, password=password).one()
+            print "***", user
+            if user:
+                if not user['status']:
+                    errors.append('pending')
+                else:
+                    r['user'] = user
+            else:
+                errors.append('info_wrong')
+        r['errors'] = errors
+        return r
+
+    def verify_vip(self, user):
+        vip = Query
+
+    def signup_person(self, email, password, confirm, gender,\
+            lastname, firstname):
+        '''
+        gender - 0:male , 1:female
+        if success return None, failed return errors.
+        '''
+        errors = []
+        if not email:
+            errors.append('email_blank')
+        if not password:
+            errors.append('password_blank')
+        if not confirm:
+            errors.append('confirm_blank')
+        if not gender in '01':
+            errors.append('gender_blank')
+        if not lastname:
+            errors.append('lastname_blank')
+        if not firstname:
+            errors.append('firstname_blank')
+
+        if email and password and confirm:
+            if password != confirm:
+                errors.append('confirm_failed')
+            if Query(User).filter_by(email=email).one():
+                errors.append('email_exists')
+        if errors:
+            return errors
+        else:
+            cid = 1
+            user_id = User(email, hashlib.md5(password).hexdigest(), cid).add()
+            PersonInfo(gender, lastname, firstname, user_id).add()
+            return None
+
