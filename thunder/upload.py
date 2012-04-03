a5a2f9e0137ce6333ab846201010a45498ab3e94
Wenkai Liu <wenkai.mel@gmail.com>
2012-04-03 21:13:27 +0800

    add upload, image, and utils

--- thunder/upload.py
+++ thunder/upload.py
@@ -1,4 +1,3 @@
-import StringIO
 import time
 from thunder.utils import random_str
 
@@ -11,19 +10,40 @@ class Upload:
         try:
             import sae.storage
             s = sae.storage.Client()
-            for file in files:
+            obj = sae.storage.Ojbect(self.files[0]['body'])
+            obj_name = int(time.time()) + random_str(8) + \
+                    self.files[0]['format'].lower()
+            s.put(self.domain, obj_name, obj)
+            return True
+        except ImportError:
+            obj = self.files[0]['body']
+            obj_name = int(time.time()) + random_str(8) + \
+                    self.files[0]['format'].lower()
+            obj_url = '/static/upload/' + obj_name
+            with open(obj_url, 'wb') as f:
+                f.write(obj)
+        except:
+            return False
+
+    def upload_all(self, **kwargs):
+        try:
+            import sae.storage
+            s = sae.storage.Client()
+            for file in self.files:
                 obj = sae.storage.Object(file['body'])
                 obj_name = int(time.time()) + random_str(8) + \
                         file['format'].lower()
-                s.put(domain, obj_name, obj)
-                obj_url = s.url(domain, obj_name)
+                s.put(self.domain, obj_name, obj)
+                obj_url = s.url(self.domain, obj_name)
+            return True
         except ImportError:
-            for file in files:
+            for file in self.files:
                 obj = file['body']
                 obj_name = int(time.time()) + random_str(8) + \
                         file['format'].lower()
                 obj_url = '/static/upload/' + obj_name
                 with open(obj_url, 'wb') as f:
                     f.write(obj)
-
-        return (domain, obj_name, obj_url)
+            return True
+        except:
+            return False
