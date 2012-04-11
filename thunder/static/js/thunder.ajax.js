598d5470dbe08085bf2b549dae25c12c618cc344
Wenkai Liu <wenkai.mel@gmail.com>
2012-04-11 15:19:43 +0800

    set X-XSRFToken for AJAX

---
+++ thunder/static/js/thunder.ajax.js
@@ -0,0 +1,21 @@
+$(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+        return r ? r[1] : undefined;
+    }
+    function sameOrigin(url) {
+        var host = document.location.host;
+        var portocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == origin + '/') ||
+        !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^GET|HEAD|OPTIONS|TRACE)$/.test(method);
+    }
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-XSRFToken", getCookie('_xsrf'));
+    }
+});
